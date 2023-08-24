f68d7376ae67c556a5c0fe3ccbaef775044f0381
Medet Ahmetson <admin@blocklords.io>
2023-08-25 01:07:25 +0300

    rename the handler test title

--- base/handler_test.go
+++ base/handler_test.go
@@ -255,6 +255,6 @@ func (test *TestHandlerSuite) Test_14_Run() {
 
 // In order for 'go test' to run this suite, we need to create
 // a normal test function and pass our suite to suite.Run
-func TestReplyController(t *testing.T) {
+func TestHandler(t *testing.T) {
 	suite.Run(t, new(TestHandlerSuite))
 }
